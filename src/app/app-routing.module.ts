87144f57b8acabece1e06f5dba3d8454cce411cc
Daniel Alejandro Rojas Santana <rojasdaniel21@gmail.com>
2019-09-08 22:31:00 -0500

    Se agrega grunt y archivo de configuración para generar el WAR de la carpeta dist del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,33 @@
-import { ModuleWithProviders } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { ModuleWithProviders } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { ManageClientComponent } from './manage-client/manage-client.component';
-import { ComingSoonComponent } from './coming-soon/coming-soon.component';
-import { ErrorComponent } from './error/error.component';
+import { ManageClientComponent } from "./manage-client/manage-client.component";
+import { ComingSoonComponent } from "./coming-soon/coming-soon.component";
+import { ErrorComponent } from "./error/error.component";
+import { environment } from 'src/environments/environment';
 
 const APP_ROUTES: Routes = [
-    // { path: '', component: ManageClientComponent },
-    { path: '', pathMatch: 'full', redirectTo: 'client-management' },
-    { path: 'client-management', component: ManageClientComponent },
-    { path: 'client-look-history', component: ComingSoonComponent },
-    { path: 'emergency-pin-config', component: ComingSoonComponent },
-    { path: 'emergency-pin-history', component: ComingSoonComponent },
-    { path: '**', component: ErrorComponent }
-]
+  // { path: '', component: ManageClientComponent },
+  { path: "", pathMatch: "full", redirectTo: "client-management" },
+  { path: "client-management", component: ManageClientComponent },
+  { path: "client-look-history", component: ComingSoonComponent },
+  { path: "emergency-pin-config", component: ComingSoonComponent },
+  { path: "emergency-pin-history", component: ComingSoonComponent },
+  { path: "**", component: ErrorComponent }
+];
 
 export const appRoutingProviders: any[] = [];
-export const APP_ROUTING: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
+export const APP_ROUTING: ModuleWithProviders = RouterModule.forRoot(
+  APP_ROUTES
+);
+@NgModule({
+  imports: [
+    RouterModule.forRoot(APP_ROUTES, {
+      useHash: true, // change useHash to true
+      enableTracing: !environment.production
+    })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
